ce1ec31dff355356318b56f1aefe004c165b6c75
akosmo <akosmo@scify.org>
2022-11-17 16:50:23 +0200

    added bootstrap

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <title>Hello World</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="alert alert-success" role="alert">
+        This is a success alert—check it out!
+    </div>
+</div>
+
+</body>
+</html>
