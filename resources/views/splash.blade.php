93808e64709d366752603244cd20f9d94d268458
Manos Codex <konem@scify.org>
2022-12-15 18:32:56 +0200

    Upd: PH-95 Bug fixes for temporary splash

--- resources/views/splash.blade.php
+++ resources/views/splash.blade.php
@@ -1,8 +1,8 @@
-<x-layoutBlank :title="'Ταξιδιώτες | Προσεχώς'" :description="'Ταξιδιώτες: Προσεχώς από τη SciFy'">
+<x-layoutBlank :title="'Ταξιδιώτες | Προσεχώς'" :description="'Ταξιδιώτες: Προσεχώς από τη SciFY'">
 
     <div class="container-fluid">
         <div class="d-flex flex-auto">
-            <img class="splash" src="{{ asset('images/logo.svg') }}" width="100%" alt="Ταξιδιώτες" />
+            <img class="splash" src="{{ asset('images/logo.svg') }}" width="100%" alt="Ταξιδιώτες" style="display: none;" />
         </div>
     </div>
 
