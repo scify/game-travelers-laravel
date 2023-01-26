465266cb75d10993413d30b2552d7423393dfc92
Paul Isaris <paulisaris@gmail.com>
2023-01-26 16:11:05 +0200

    Refactoring

--- app/Http/Middleware/EnsureIdsAreValid.php
+++ app/Http/Middleware/EnsureIdsAreValid.php
@@ -5,17 +5,17 @@ namespace App\Http\Middleware;
 use App\Repository\Game\GameRepository;
 use App\Repository\Player\PlayerRepository;
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
-class EnsureIdsAreValid
-{
+class EnsureIdsAreValid {
 
     protected PlayerRepository $playerRepository;
     protected GameRepository $gameRepository;
 
 
-    public function __construct(PlayerRepository $playerRepository, GameRepository $gameRepository)
-    {
+    public function __construct(PlayerRepository $playerRepository, GameRepository $gameRepository) {
         $this->playerRepository = $playerRepository;
         $this->gameRepository = $gameRepository;
     }
@@ -23,38 +23,37 @@ class EnsureIdsAreValid
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @param Request $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return Response|RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
-    {
+    public function handle(Request $request, Closure $next) {
         $parameters = $request->route()->parameters;
-        $player_id = (int) $parameters['player_id'];
-        $game_id = (int) $parameters['game_id'];
+        $player_id = (int)$parameters['player_id'];
+        $game_id = (int)$parameters['game_id'];
         if ($player_id == 0)
             return $next($request);
-        else {
-            $problem_found = false;
-            $user_id = auth()->user()->id;
-            $entry = $this->playerRepository->allWhere(['id' => $player_id],['user_id'])->all();
-            if (sizeof($entry) == 0)
+
+        $problem_found = false;
+        $user_id = auth()->id();
+        $entry = $this->playerRepository->allWhere(['id' => $player_id], ['user_id']);
+        if ($entry->count() == 0)
+            $problem_found = true;
+        else if ($entry[0]->user_id != $user_id)
+            $problem_found = true;
+
+        if ($game_id != 0) {
+            $entry = $this->gameRepository->allWhere(['id' => $game_id, 'active' => true], ['user_id']);
+            if ($entry->count() == 0)
                 $problem_found = true;
             else if ($entry[0]->user_id != $user_id)
                 $problem_found = true;
-
-            if ($game_id != 0) {
-                $entry = $this->gameRepository->allWhere(['id' => $game_id, 'active' => true], ['user_id'])->all();
-                if (sizeof($entry) == 0)
-                    $problem_found = true;
-                else if ($entry[0]->user_id != $user_id)
-                    $problem_found = true;
-            }
-
-            if ($problem_found)
-                return abort(403, 'Unauthorized action.');
-            else
-                return $next($request);
         }
+
+        if ($problem_found)
+            abort(403, 'Unauthorized action.');
+        else
+            return $next($request);
+
     }
 }
