944cb4d4beac6644bd4047ca90d50897e6d5bbb0
Manos Codex <100481023+codeuxius@users.noreply.github.com>
2023-01-31 06:11:48 +0200

    New: Curvy landing footers.

--- resources/views/privacy-policy/page.blade.php
+++ resources/views/privacy-policy/page.blade.php
@@ -3,8 +3,8 @@
     :description="'This is the privacy policy page'"
 >
 
-<div class="landing bg-light px-4 pb-5 my-2">
-    <div class="container-lg p-4">
+<div class="landing bg-light px-4">
+    <div class="container-lg pt-4 px-4">
         @include('privacy-policy.content-' . app()->getLocale())
     </div>
 </div>
