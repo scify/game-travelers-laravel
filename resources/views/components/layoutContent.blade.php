a9a1c72722ccf897aeb73a5237dd63728cc65f5a
Manos Codex <100481023+codeuxius@users.noreply.github.com>
2023-01-31 06:11:48 +0200

    New: Curvy landing footers.

--- resources/views/components/layoutContent.blade.php
+++ resources/views/components/layoutContent.blade.php
@@ -6,6 +6,8 @@
     * force the content to be rendered in the absolute middle of the viewport.
     * Used on all content pages: e.g. Landing (aka Index), Privacy Policy,
     * Cookies etc. See the default layout.blade.php for optional parameters.
+    * Note that this layout does not support and does not include Vue.js or
+    * all the custom-made global window.Laravel JS dependencies.
     --}}
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
@@ -18,17 +20,11 @@
     @yield('css', '') {{-- Optional: Additional CSS stylesheets, if needed. --}}
 </head>
 <body>
-    <div class="travelers">
-        <div class="travelers-container">
-            @include('layout.header')
-            <section class="content">
-                {{ $slot }}
-            </section>
-            @include('layout.landing.footer')
-        </div>
-    </div>
-    @include('layout.footer-scripts')
+    @include('layout.landing.header')
+    <section class="content-non-interactive mb-n2">
+        {{ $slot }}
+    </section>
+    @include('layout.landing.footer')
     @yield('scripts', '') {{-- Optional: JS scripts loaded after DOM is rendered --}}
-    @isset($hasVue)<script src="{{ mix('js/vue.js') }}" defer></script>@endisset
 </body>
 </html>
--- resources/views/layout/landing/footer.blade.php
+++ resources/views/layout/landing/footer.blade.php
@@ -1,12 +1,82 @@
 <!-- /resources/views/layout/landing/footer.blade.php -->
+<div class="trvl-landing-footer-curve bg-light" aria-role="decoration">
+    <div class="trvl-landing-footer-curve--dash">
+        &nbsp;
+    </div>
+</div>
 <footer class="trvl-landing-footer bg-green">
     <div class="container-xxl p-4">
-        <div class="scify">
-            <a href="http://www.scify.org" target="_blank"><img
-                srcset="{{ asset('images/SciFY@2x.png') }} 2x"
-                src="{{ asset('images/SciFY.png') }}"
-                width="105" height="53"
-                alt="Created by SciFY"></a>
+
+        <div class="d-flex flex-column flex-sm-row pt-5">
+
+            <div class="trvl-landing-footer--links flex-grow-1">
+                <ul>
+                    <li>Επικοινώνησε μαζί μας</li>
+                    <li>Cookies Policy</li>
+                    <li>
+                        <a href="{{ route('privacy-policy') }}">
+                            {{ __("messages.privacy_policy")}}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="trvl-landing-footer--social pt-5 pt-sm-0">
+                <div class="social-container">
+                    <u>Ακολούθησέ μας</u>
+                    <ul class="social-links">
+                        <li class="social-links--linkedin">
+                            <a href="https://linkedin.com/company/scify-not-for-profit-company" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    loading="lazy"
+                                    srcset="{{ asset('images/landing/linkedin@2x.png') }} 2x"
+                                    src="{{ asset('images/landing/linkedin.png') }}"
+                                    width="36" height="36"
+                                    alt="Connect with us on LinkedIn"
+                                >
+                            </a>
+                        </li>
+                        <li class="social-links--twitter">
+                            <a href="https://twitter.com/scify_org" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    loading="lazy"
+                                    srcset="{{ asset('images/landing/twitter@2x.png') }} 2x"
+                                    src="{{ asset('images/landing/twitter.png') }}"
+                                    width="36" height="30"
+                                    alt="Follow us on Twitter"
+                                >
+                            </a>
+                        </li>
+                        <li class="social-links--facebook">
+                            <a href="https://facebook.com/SciFY.org" target="_blank" rel="noopener noreferrer">
+                                <img
+                                    loading="lazy"
+                                    srcset="{{ asset('images/landing/facebook@2x.png') }} 2x"
+                                    src="{{ asset('images/landing/facebook.png') }}"
+                                    width="35" height="35"
+                                    alt="Find us on Facebook"
+                                >
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
+        <div class="d-flex pt-5">
+
+            <div class="scify">
+                <a href="http://www.scify.org" target="_blank">
+                    <img
+                        srcset="{{ asset('images/SciFY@2x.png') }} 2x"
+                        src="{{ asset('images/SciFY.png') }}"
+                        width="105" height="53"
+                        alt="Created by SciFY"
+                    >
+                </a>
+            </div>
+
+        </div>
+
     </div>
 </footer>
