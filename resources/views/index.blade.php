04c1d42baeb1ff1ccae0f9018cc78654e4e7f836
Manos Codex <100481023+codeuxius@users.noreply.github.com>
2023-02-02 04:01:24 +0200

    New: Landing Section 4 - Screenshots

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,10 @@
     :title="__('messages.app_name')"
     :header-background-color="'white'"
 >
+
+    <!-- section--tree-forest -->
     <section class="landing bg-green px-4">
-        <div class="container-lg pt-2 px-4 pb-5">
+        <div class="container-lg pt-2 px-4 pb-6 pb-lg-7">
             <div class="landing-trees d-flex justify-content-center">
                 <img class="img-xl"
                     loading="lazy"
@@ -35,70 +37,69 @@
             <div class="landing-title pt-4">
                 <h1>
                     <span>Ταξίδι στο παιχνίδι,</span>
-                    <span>για όλα τα παιδιά!</span></h1>
+                    <span>για όλα τα παιδιά!</span>
+                </h1>
             </div>
             <div class="landing-call-to-action pt-4 text-center">
-                <a href="{{ route('login') }}" class="btn btn-primary btn-lg text-nowrap responsive-expand" tabindex="1">
+                <a href="{{ route('login') }}" class="btn btn-primary btn-lg text-nowrap responsive-expand">
                     παίξε
                 </a>
             </div>
         </div>
     </section>
 
-    {{--  @todo replace with proper curve when exported | hidden >= lg
-        MISSING CURVE for <LG viewports, there's nothing to see here atm.
-        --}}
-    {{--
-    <div aria-role="presentation" class="d-lg-none trvl-curve trvl-curve--header trvl-curve--header--color trvl-curve--header--color--white bg-green">
-        <div class="trvl-curve trvl-curve--header trvl-curve--header--dash">
-            &nbsp;
-        </div>
-    </div>
-    --}}
-
-    <section class="landing bg-light bg-lg-green px-4 pt-6 mb-lg-n350">
-        <div class="container-lg pt-2 px-4">
-            <div class="landing-blocks landing-blocks--three">
-                <div class="d-flex flex-column flex-lg-row justify-content-around align-items-center">
-                    <div class="landing-block landing-block--abstract">
-                        <img class=""
-                            loading="lazy"
-                            srcset="{{ asset('images/landing/blocks/balloon@2x.png') }} 2x"
-                            src="{{ asset('images/landing/blocks/balloon.png') }}"
-                            width="264" height="398"
-                            aria-role="presentation"
-                            alt=""
-                        >
-                        <div class="landing-block--label">
-                            <span>Ευκαιρία για παιχνίδι</span>
-                            <span>και χαρά</span>
+    <!-- section--three-blocks -->
+    <section class="landing trvl-curve trvl-curve--landing-one-lg--color--green bg-light bg-lg-green px-0 pt-6-xl mb-lg-n350">
+        <div class="trvl-curve trvl-curve--landing-three-blocks--sm-lg-dash px-4">
+            <div class="container-lg pt-2 px-4">
+                <div class="landing-blocks landing-blocks--three">
+                    <div class="d-flex flex-column flex-lg-row justify-content-around align-items-center">
+                        <div class="landing-block landing-block--abstract">
+                            <img class=""
+                                loading="lazy"
+                                srcset="{{ asset('images/landing/blocks/balloon@2x.png') }} 2x"
+                                src="{{ asset('images/landing/blocks/balloon.png') }}"
+                                width="264" height="398"
+                                aria-role="presentation"
+                                alt=""
+                            >
+                            <div class="landing-block--label">
+                                <h2>
+                                    <span>Ευκαιρία για παιχνίδι</span>
+                                    <span>και χαρά</span>
+                                </h2>
+                            </div>
                         </div>
-                    </div>
-                    <div class="landing-block landing-block--abstract mt-6 mt-lg-0">
-                        <img class=""
-                            loading="lazy"
-                            srcset="{{ asset('images/landing/blocks/unicorn@2x.png') }} 2x"
-                            src="{{ asset('images/landing/blocks/unicorn.png') }}"
-                            width="264" height="398"
-                            aria-role="presentation"
-                            alt=""
-                        >
-                        <div class="landing-block--label">
-                            <span>Ενισχύει την αυτονομία</span>
-                            <span>και την αυτοπεποίθηση<span>
+                        <div class="landing-block landing-block--abstract mt-6 mt-lg-0">
+                            <img class=""
+                                loading="lazy"
+                                srcset="{{ asset('images/landing/blocks/unicorn@2x.png') }} 2x"
+                                src="{{ asset('images/landing/blocks/unicorn.png') }}"
+                                width="264" height="398"
+                                aria-role="presentation"
+                                alt=""
+                            >
+                            <div class="landing-block--label">
+                                <h2>
+                                    <span>Ενισχύει την αυτονομία</span>
+                                    <span>και την αυτοπεποίθηση<span>
+                                </h2>
+                            </div>
                         </div>
-                    </div>
-                    <div class="landing-block landing-block--abstract mt-6 mt-lg-0">
-                        <img class=""
-                            loading="lazy"
-                            srcset="{{ asset('images/landing/blocks/vase@2x.png') }} 2x"
-                            src="{{ asset('images/landing/blocks/vase.png') }}"
-                            width="264" height="398"
-                            aria-role="presentation"
-                            alt=""
-                        >
-                        <div class="landing-block--label">
-                            <span>Ενισχύει τη μάθηση</span>
+                        <div class="landing-block landing-block--abstract mt-6 mt-lg-0">
+                            <img class=""
+                                loading="lazy"
+                                srcset="{{ asset('images/landing/blocks/vase@2x.png') }} 2x"
+                                src="{{ asset('images/landing/blocks/vase.png') }}"
+                                width="264" height="398"
+                                aria-role="presentation"
+                                alt=""
+                            >
+                            <div class="landing-block--label">
+                                <h2>
+                                    <span>Ενισχύει τη μάθηση</span>
+                                </h2>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -106,15 +107,14 @@
         </div>
     </section>
 
-    {{--  @todo replace with proper curve when exported | hidden < lg
-            MISSING PROPER CURVE FOR >=LG SCREENS (this stands as a replacement)
-    --}}
-    <div aria-role="presentation" class="d-none d-lg-block trvl-curve trvl-curve--landing-one trvl-curve--landing-one--color trvl-curve--landing-one--color--green bg-light">
-        <div class="trvl-curve trvl-curve--landing-one trvl-curve--landing-one--dash">
+    <!-- curve--landing-one-xl -->
+    <div aria-role="presentation" class="d-none d-lg-block trvl-curve trvl-curve--landing-one-xl trvl-curve--landing-one-xl--color trvl-curve--landing-one-xl--color--green bg-light">
+        <div class="trvl-curve trvl-curve--landing-one-xl trvl-curve--landing-one-xl--dash">
             &nbsp;
         </div>
     </div>
 
+    <!-- section--landing-numbers -->
     <section class="landing bg-light px-4 pt-0 pt-lg-4 pt-lg-6">
         <div class="container-lg pt-2">
             <div class="landing-numbers py-1 py-md-6 d-flex flex-column justify-content-around ">
@@ -127,7 +127,9 @@
                         aria-role="presentation"
                         alt=""
                     >
-                    <div class="ps-0 ps-md-4 pt-4 pt-md-0">Για παιδιά με αναπηρία</div>
+                    <div class="ps-0 ps-md-4 pt-4 pt-md-0">
+                        <h2>Για παιδιά με αναπηρία</h2>
+                    </div>
                 </div>
                 <div class="landing-numbers--two d-flex flex-column flex-lg-row align-items-center" >
                     <img class=""
@@ -138,7 +140,9 @@
                         aria-role="presentation"
                         alt=""
                     >
-                    <div class="ps-0 ps-md-4 pt-4 pt-md-0">Για φορείς φροντίδας ΑμεΑ</div>
+                    <div class="ps-0 ps-md-4 pt-4 pt-md-0">
+                        <h2>Για φορείς φροντίδας ΑμεΑ</h2>
+                    </div>
                 </div>
                 <div class="landing-numbers--three d-flex flex-column flex-lg-row align-items-center ">
                     <img class=""
@@ -149,7 +153,56 @@
                         aria-role="presentation"
                         alt=""
                     >
-                    <div class="ps-0 ps-md-4 pt-4 pt-md-0">...και για όλους!</div>
+                    <div class="ps-0 ps-md-4 pt-4 pt-md-0">
+                        <h2>...και για όλους!</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- curve--landing-two -->
+    <div aria-role="presentation" class="trvl-curve trvl-curve--landing-two trvl-curve--landing-two--color trvl-curve--landing-two--color--brown bg-light">
+        <div class="">
+            &nbsp;
+        </div>
+    </div>
+
+    <!-- section--screenshots -->
+    <section class="landing bg-brown px-4 pt-4 pb-6">
+        <div class="container-lg pb-4">
+            <div class="landing-screenshots justify-content-start py-6">
+                <div class="screenshot px-0 px-sm-5 px-xl-0 pb-5">
+                    <div class="offset-xl-0">
+                        <h2>
+                            <span>Προσαρμόζεται πλήρως</span>
+                            <span>στις ανάγκες του παιδιού</span>
+                        </h2>
+                    </div>
+                    <div class="pt-2 offset-xl-1">
+                        <img class="img-fluid screenshot rounded"
+                            loading="lazy"
+                            srcset="{{ asset('images/landing/screenshots/screenshot-01@2x.png') }} 2x"
+                            src="{{ asset('images/landing/screenshots/screenshot-01.png') }}"
+                            width="760" height="427"
+                            alt="Εικόνα στην οποία απεικονίζονται οι ρυθμίσεις του παιχνιδιού οι οποίες σχετίζονται με τον τρόπο πλοήγησης σε αυτό. Εμφανίζονται οι ρυθμίσεις για το στυλ πλοήγησης (έχει επιλεχθεί «αυτόματο»), την ταχύτητα σάρωσης (8 δευτερόλεπτα) και τη βοήθεια μετά από αριθμό λαθών (έχει επιλεχθεί 1)."
+                        >
+                    </div>
+                </div>
+                <div class="screenshot pt-6 px-0 px-sm-5 px-xl-0">
+                    <div class="pt-2 offset-xl-4 text-center">
+                        <h2>
+                            <span>Διαφορετικό προφίλ</span>
+                            <span>για κάθε παίκτη</span>
+                        </h2>
+                        <img class="img-fluid screenshot rounded pt-2"
+                            loading="lazy"
+                            srcset="{{ asset('images/landing/screenshots/screenshot-02@2x.png') }} 2x"
+                            src="{{ asset('images/landing/screenshots/screenshot-02.png') }}"
+                            width="760" height="427"
+                            alt="Εικόνα στην οποία εμφανίζεται η οθόνη επιλογής προφίλ παίκτη στο παιχνίδι. Παρουσιάζονται συνολικά 14 προφίλ, όπως επίσης και το πλήκτρο που επιτρέπει τη δημιουργία ακόμη περισσότερων."
+                        >
+                    </div>
                 </div>
             </div>
         </div>
