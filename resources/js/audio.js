5601d8d0a73bb171e255816f23ce52535f015645
Manos Codex <100481023+codeuxius@users.noreply.github.com>
2023-02-10 02:49:55 +0200

    PoC: audio.js now supports custom player sounds

--- resources/js/audio.js
+++ resources/js/audio.js
@@ -71,6 +71,50 @@
 	 * // Stops any other sound(s) and immediately start playing welcome_1.mp3
 	 */
 	const sound = function (audiofile, callback = null, interrupt = false) {
+		/** Checks for custom player files, based on the audiofile provided. */
+		function getPlayerAudioFile(audiofile) {
+			// Custom Player Audio Files Support
+			if (window.Laravel.playerAudioFiles && window.Laravel.playerUrl) {
+				var playerFolders = audiofile.split(".");
+				// We are only interested in the last part (for now)...
+				var lastPart = playerFolders[playerFolders.length - 1];
+				// and only if that last part ends with a number (for now)...
+				var lastMatchNumber = lastPart.match(/(\d+)/);
+				if (lastMatchNumber) {
+					var lastNumber = lastMatchNumber[1];
+					var possibleMatches = [];
+					for (var i = 1; i <= lastNumber; i++) {
+						var modifiedLastPart = lastPart.replace(lastNumber, i);
+						playerFolders[playerFolders.length - 1] =
+							modifiedLastPart;
+						var playerAudioFound = playerFolders.reduce(
+							(obj, key) => obj && obj[key],
+							window.Laravel.playerAudioFiles
+						);
+						if (playerAudioFound) {
+							possibleMatches.push(playerAudioFound);
+						}
+					}
+					if (possibleMatches.length > 0) {
+						var randomPlayerIndex = Math.floor(
+							Math.random() * possibleMatches.length
+						);
+						return possibleMatches[randomPlayerIndex];
+					}
+				} else {
+					var playerAudioNoDigitFound = playerFolders.reduce(
+						(obj, key) => obj && obj[key],
+						window.Laravel.playerAudioFiles
+					);
+					if (playerAudioNoDigitFound) {
+						return playerAudioNoDigitFound;
+					}
+				}
+			}
+			// If nothing found...
+			return false;
+		}
+
 		if (!window.Laravel.audioFiles) {
 			console.log("Audio files not found");
 			return;
@@ -110,9 +154,23 @@
 			console.log("Sound file not found");
 			return;
 		}
-		var filename = folders.pop() + ".mp3";
-		var folderPath = "/audio/" + folders.join("/");
+		// So, is there a custom player sound?
+		var playerAudioFile = getPlayerAudioFile(audiofile);
+		var filename, folderPath;
+		if (playerAudioFile) {
+			// Play the custom player sound...
+			// e.g. /player/b5b72dd79161d837bef10dd3d54de385/welcome_1.mp3
+			filename = playerAudioFile;
+			folderPath = window.Laravel.playerUrl;
+		} else {
+			// Play the default sound...
+			filename = folders.pop() + ".mp3";
+			folderPath = "/audio/" + folders.join("/");
+		}
+		// @todo remove console.logs from this file
+		console.log(`Playing audio: ${folderPath}/${filename}`);
 		var audio = new Audio(folderPath + "/" + filename);
+
 		// console.log(folders.pop()); // e.g. game_start
 		window.travelersSounds.push({ audio: audio, file: filename });
 
