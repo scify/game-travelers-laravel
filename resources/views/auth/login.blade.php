3490f41a0044e8b2ae6853712299141cd5f645b1
Manos Codex <100481023+codeuxius@users.noreply.github.com>
2023-02-20 17:35:51 +0200

    Fix: Minor fixes for login with responsive alert

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     @endsection
 
     <!-- login content -->
-    <div class="section background background-group-1 container-xxl px-4">
+    <div class="section background background-group-1 responsive-hide-completely container-xxl px-4">
         <div class="row">
             <div class="col-md-3 order-md-2">
                 <div class="ps-4 ps-md-0 pt-4">
@@ -64,7 +64,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="mx-4 mt-4 p-4 alert alert-note progressive-alert d-flex align-items-center" role="alert">
+                        <div class="ms-4 me-4 ms-lg-0 mt-4 p-4 alert alert-note progressive-alert d-flex align-items-center" role="alert">
                             <svg xmlns="http://www.w3.org/2000/svg" class="bi flex-shrink-0 me-4" role="img" aria-label="{{ __("messages.alert_notice") }}:" viewBox="0 0 16 16">
                                 <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
                             </svg>
