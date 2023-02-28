32c5b085a6fac9aa8a49beb7c1f257f3b6ce2819
Manos Codex <100481023+codeuxius@users.noreply.github.com>
2023-02-28 17:15:47 +0200

    Fix: passwordRequestSuccess footer

--- lang/el/notifications.php
+++ lang/el/notifications.php
@@ -10,10 +10,10 @@ return [
     */
 
     'registration_subject' => 'Taxidiotes game | Καλώς όρισες!',
-    'registration_greeting' => 'Καλως όρίσες στο παιχνίδι Ταξιδώτες!',
+    'registration_greeting' => 'Καλώς όρισες στο παιχνίδι «Ταξιδιώτες»!',
     'registration_prompt' => 'Ξεκίνησε το παιχνίδι!',
     'registration_end' => 'Καλή διασκέδαση',
     'regards' => 'Φιλικά',
-    'trouble_with_button' => 'Αν έχετε πρόβλημα να πατήσετε το ',
-    'copy_paste_url' => ' κουμπί, αντιγράψτε και επικολλήστε το παρακάτω URL στο web browser σας:',
+    'trouble_with_button' => 'Αν έχετε πρόβλημα να πατήσετε το κουμπί',
+    'copy_paste_url' => ' , αντιγράψτε και επικολλήστε το παρακάτω URL στο web browser σας:',
 ];
