a6bb565c2079b34dceeef3f8bee4c64d2dbfefc8
Manos Codex <100481023+codeuxius@users.noreply.github.com>
2023-03-02 16:05:47 +0200

    Upd: Carousel, logos, meta-tags
    - Carousel: Colours & behaviour on small screen adjustments
    - Carousel: Added two slides with actual images and quotes
    - Logos: 3x versions for Landing Page header and footer
    - Meta-tags: Added descriptions & creator for Twitter, Facebook
    
    Various minor bug-fixes.

--- resources/views/layout/meta.blade.php
+++ resources/views/layout/meta.blade.php
@@ -9,10 +9,12 @@
 <meta name="og:type" property="og:type" content="website">
 <meta name="og:url" property="og:url" content="{{ url()->current() }}">
 <meta name="og:image" property="og:image" content="{{ asset('images/taxidiotes_logo.webp') }}">
-<meta name="og:description" property="og:description" content="{{ __('messages.meta_description') }}">
+<meta name="og:description" property="og:description" content="{{ $description ?? __('messages.meta_description') }}">
 <meta name="og:site_name" property="og:site_name" content="{{ __('messages.app_name') }}">
 <meta name="twitter:card" content="summary">
 <meta name="twitter:title" content="{{ $title ?? __('messages.app_name') }}">
-<meta name="twitter:description" content="{{ __('messages.meta_description') }}">
+<meta name="twitter:description" content="{{ $description ?? __('messages.meta_description') }}">
 <meta name="twitter:image" content="{{ asset('images/taxidiotes_logo.webp') }}">
+<meta name="twitter:image:alt" content="{{ __('messages.meta_logo_image_alt')}}">
+<meta name="twitter:creator" content="@SciFY_org">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
